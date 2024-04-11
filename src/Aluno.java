bcff94553ab4530cbaf63c119e893b771928b671
Guilherme Figueirêdo <guilherme.figueiredosc@gmail.com>
2024-04-11 15:23:06 -0300

    projeto quase pronta (considerando que n vão ser usadas funções)

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,9 +1,16 @@
+import Disciplinas.ListaMaterias;
 import Disciplinas.Materia;
 
 public class Aluno {
     private String nome;
     private int  rgm;
-    private Materia materias;
+    private ListaMaterias listaMaterias;
+
+    public Aluno(String nome, int rgm, ListaMaterias listaMaterias) {
+        this.nome = nome;
+        this.rgm = rgm;
+        this.listaMaterias = listaMaterias;
+    }
 
     public Aluno(String nome, int rgm) {
         this.nome = nome;
@@ -26,12 +33,20 @@ public class Aluno {
         this.nome = nome;
     }
 
+    public ListaMaterias getListaMaterias() {
+        return listaMaterias;
+    }
+
+    public void setListaMaterias(ListaMaterias listaMaterias) {
+        this.listaMaterias = listaMaterias;
+    }
+
     @Override
     public String toString() {
         return "Aluno{" +
                 "nome='" + nome + '\'' +
                 ", rgm=" + rgm +
-                ", materias=" + materias +
+                ", materias=" + listaMaterias +
                 '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,72 @@
+import Disciplinas.ListaMaterias;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Quantos alunos adicionar na lista?");
+        System.out.print("Quantos alunos adicionar na lista? ");
         int tamanho = input.nextInt();
 
-        Aluno[] listaAlunos = new Aluno[tamanho];
+        Aluno[] vetorAlunos = new Aluno[tamanho];
 
         if (tamanho > 60 || tamanho < 0) {
             System.out.println("Tamanho inválido!");
             System.exit(0);
         }
 
-        for (int i = 0; i < listaAlunos.length; i++){
+        for (int i = 0; i < vetorAlunos.length; i++){
+            ListaMaterias listaMaterias = new ListaMaterias();
 
-            System.out.println("Digite o nome: ");
+            System.out.printf("  Digite o nome do aluno[%d]: ", i+1);
             String nome = input.next();
 
-            System.out.println("Digite o rgm: ");
+            System.out.printf("  Digite o rgm do aluno[%d]: ", i+1);
             int rgm = input.nextInt();
 
-            listaAlunos[i] = new Aluno(nome, rgm);
+            input.nextLine();
+
+            while (true){
+                System.out.printf("    Digite uma matéria: ");
+                String materia = input.nextLine();
+                listaMaterias.addMateria(materia);
+
+                System.out.print("    Quer adicionar mais matérias [sim/não]? ");
+                String resposta = input.nextLine().toLowerCase();
+
+                if (resposta.equals("não")) {
+                    break;
+                }
+            }
+
+            vetorAlunos[i] = new Aluno(nome, rgm, listaMaterias);
         }
 
-        System.out.println("Digite um rgm para buscar o aluno na lista: ");
+        System.out.println("\nLista com todos os alunos e seu dados: ");
+        for (int i = 0; i < vetorAlunos.length; i++){
+            System.out.println(" Nome: " + vetorAlunos[i].getNome());
+            System.out.println(" RGM: " + vetorAlunos[i].getRgm());
+            System.out.println(" Matéria/as: " + vetorAlunos[i].getListaMaterias());
+        }
+
+        System.out.print("\nDigite um rgm para buscar o aluno na lista: ");
         int buscarRgm = input.nextInt();
 
-        for (int i = 0; i < listaAlunos.length; i++){
-            if (buscarRgm == listaAlunos[i].getRgm()){
-                System.out.println("Nome: " + listaAlunos[i].getNome());
-                System.out.println("RGM: " + listaAlunos[i].getRgm());
+        boolean alunoEncontrado = false;
+        for (int i = 0; i < vetorAlunos.length; i++) {
+            if (buscarRgm == vetorAlunos[i].getRgm()) {
+                System.out.println(" Nome: " + vetorAlunos[i].getNome());
+                System.out.println(" RGM: " + vetorAlunos[i].getRgm());
+                System.out.println(" Matéria/as: " + vetorAlunos[i].getListaMaterias());
+                alunoEncontrado = true;
+                break;
             }
+        }
+        if (!alunoEncontrado) {
+            System.out.println("Aluno não encontrado! ");
         }
 
         input.close();
-
-
-//        ListaAlunos lista = new ListaAlunos();
-//
-//
-//        lista.adicionarAluno(123,"jose");
-//        lista.adicionarAluno(1234,"gui");
-//        lista.adicionarAluno(525, "Igor");
-//        lista.adicionarAluno(525555, "Matheus");
-//
-//
-//        System.out.println(lista);
-//
-//        lista.removerAluno(123);
-//
-//        System.out.println(lista);
-//
-//        sc.close();
-
     }
 }
